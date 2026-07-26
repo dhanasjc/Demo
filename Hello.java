7aa60a388575037bfbe7e5de721cdab0401f5c7e
Dhanasekar <dhanasjc@gmail.com>
2026-07-26 22:22:44 +0530

    first commit

--- Hello.java
+++ Hello.java
@@ -1,3 +1,12 @@
+
+public class Hello {
 public static void main(String[] args) {
     System.out.println("Hello, World!");
+    printhello();
+    
+}
+
+private static void printhello() {
+    System.out.println("Hello from the printhello method!");
+}
 }
